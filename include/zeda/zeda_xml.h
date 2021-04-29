ab161ea9ed657aa8cb83086f7fdca24433914464
Tomomichi Sugihara <zhidao@ieee.org>
2021-04-29 12:35:10 +0900

    Added zeda_xml. Modified specification of zeda-config-gen.

---
+++ include/zeda/zeda_xml.h
@@ -0,0 +1,64 @@
+/* ZEDA - Elementary Data and Algorithms
+ * Copyright (C) 1998 Tomomichi Sugihara (Zhidao)
+ */
+/*! \file zeda_xml.h
+ * \brief XML file operations.
+ * \author Zhidao
+ */
+
+#ifndef __ZEDA_XML_H__
+#define __ZEDA_XML_H__
+
+#ifndef __KERNEL__
+
+#include <zeda/zeda_misc.h>
+#include <libxml/parser.h>
+
+__BEGIN_DECLS
+
+/* check if the compiled and linked versions match. */
+#define zXMLInit()                LIBXML_TEST_VERSION
+
+/* strings */
+
+#define zXMLCheckStr(str1,str2)   ( strcmp( (char*)(str1), (str2) ) == 0 )
+
+#define zXMLGetString(node)       ( (char *)xmlNodeListGetString( (node)->doc, node, 1 ) )
+#define zXMLGetInt(node)          atoi( zXMLGetString( node ) )
+#define zXMLGetContent(node)      ( (char *)xmlNodeGetContent( node ) )
+
+/* nodes & elements */
+
+#define zXMLForEachNode(node,np) \
+  for( (np)=(node); (np); (np)=(np)->next )
+
+#define zXMLCheckElement(node,str) \
+  ( (node)->type == XML_ELEMENT_NODE && zXMLCheckStr( (node)->name, str ) )
+
+#define zXMLCheckElementAndExec(node,name,op) \
+  if( zXMLCheckElement(node,name) ){ if( !op ) return false; }
+
+__EXPORT xmlNode *zXMLFindNodeElement(xmlNode *node, char *name);
+#define zXMLFindNodeAttr(node,name) ( (char *)xmlGetProp( node, (xmlChar*)name ) )
+
+/* attributes */
+
+#define zXMLGetAttrVal(attr)         ( (char *)xmlNodeListGetString( (attr)->doc, (attr)->children, 1 ) )
+#define zXMLGetAttrInt(attr)         atoi( zXMLGetAttrVal( attr ) )
+#define zXMLGetAttrDouble(attr)      atof( zXMLGetAttrVal( attr ) )
+
+#define zXMLForEachAttr(node,attr) \
+  for( (attr)=(node)->properties; (attr); (attr)=(attr)->next )
+
+#define zXMLCheckAttr(attr,str)      zXMLCheckStr( (attr)->name, str )
+
+#define zXMLCheckAttrAndExec(attr,str,op) \
+  if( zXMLCheckStr( (attr)->name, str ) ){ (op); }
+
+__EXPORT bool zXMLCheckAttrVal(xmlAttr *attr, char *name, char *str);
+
+__END_DECLS
+
+#endif /* __KERNEL__ */
+
+#endif /* __ZEDA_XML_H__ */
---
+++ src/zeda_xml.c
@@ -0,0 +1,21 @@
+/* ZEDA - Elementary Data and Algorithms
+ * Copyright (C) 1998 Tomomichi Sugihara (Zhidao)
+ *
+ * XML file operations.
+ */
+
+#include <zeda/zeda_xml.h>
+
+xmlNode *zXMLFindNodeElement(xmlNode *node, char *name)
+{
+  xmlNode *np;
+
+  zXMLForEachNode( node, np )
+    if( zXMLCheckElement( np, name ) ) return np;
+  return NULL;
+}
+
+bool zXMLCheckAttrVal(xmlAttr *attr, char *name, char *str)
+{
+  return zXMLCheckAttr( attr, name ) && zXMLCheckStr( zXMLGetAttrVal(attr), str );
+}
