2dc81a8225b4131050aeb84370406985bf00a695
kdaic <develop@kdaic.net>
2022-10-23 23:23:36 +0900

    Modify example/string/read_int_test.c to compile and execute for windows

--- example/string/read_int_test.c
+++ example/string/read_int_test.c
@@ -6,11 +6,33 @@ int main(void)
 
   printf( "enter a string: " );
   if( !fgets( buf, BUFSIZ, stdin ) ) return 1;
-  while( buf[0] )
-    printf( "%d\n", zSInt( buf ) );
+#ifdef __WINDOWS__
+  int val = 0;
+  while (buf[0])
+  {
+      char ret = *zSInt(buf, &val);
+      if (ret != '\0')
+      {
+          printf("ret=%c\n", ret);
+          printf("val=%d\n", val);
+      }
+  }
+#else
+  while (buf[0])
+      printf("%d\n", zSInt(buf));
+#endif
 
   printf( "enter 3 numbers: " );
-  printf( "val[1]=%d, val[2]=%d, val[3]=%d\n", zFInt(stdin), zFInt(stdin), zFInt(stdin) );
+#ifdef __WINDOWS__  
+  int fval1=0, fval2=0, fval3=0;
+  char ret1 = *zFInt(stdin, &fval1);
+  char ret2 = *zFInt(stdin, &fval2);
+  char ret3 = *zFInt(stdin, &fval3);
+  printf("ret[1]=%c, ret[2]=%c, ret[3]=%c\n", ret1, ret2, ret3);
+  printf("val[1]=%d, val[2]=%d, val[3]=%d\n", fval1, fval2, fval3);
+#else
+  printf("val[1]=%d, val[2]=%d, val[3]=%d\n", zFInt(stdin), zFInt(stdin), zFInt(stdin));
+#endif
   printf( "(probably the result is different from what is expected due to the specification of printf.)\n" );
   return 0;
 }
