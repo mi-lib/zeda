2150859b73ab29cad81446c45172f6f0b9fc4d0b
kdaic <develop@kdaic.net>
2024-11-12 01:51:10 +0900

    Update a sample code that the usecase of num of ZTKPrp is -1 but the number of eval/fprint is greater than 0.

--- example/ztk/ztk_variable_length_tagkey_eval_and_print_test.c
+++ example/ztk/ztk_variable_length_tagkey_eval_and_print_test.c
@@ -4,51 +4,62 @@
 /* definition of a calculation expression. */
 typedef struct{
   int var_tag_num;
-  int var_key_num;
+  int* var_key_num; /* [tag] array */
+  int tag_cnt;
+  int** variable; /* [tag][key] array */
 } expression_t;
 
 #define exp_obj ((expression_t *)obj)
 
-void *eval_variable(void *obj, int i, void *arg, ZTK *ztk)
+void *eval_variable_key(void *obj, int i, void *arg, ZTK *ztk)
 {
-  int c = ZTKInt(ztk);
-  printf("key cnt=%d value=%d --- read\n", i, c);
+  int c;
+
+  c = ZTKInt(ztk);
+  printf("key cnt=%d/%d value=%d --- read\n", i, exp_obj->var_key_num[exp_obj->tag_cnt]-1, c);
+  exp_obj->variable[exp_obj->tag_cnt][i] = c;
   return obj;
 }
 
-void print_variable(FILE*fp, int i, void *obj)
+bool print_variable_key(FILE*fp, int i, void *obj)
 {
-  int j;
+  fprintf( fp, "%d\n", exp_obj->variable[exp_obj->tag_cnt][i] );
 
-  for( j=0; j < exp_obj->var_key_num; j++ ){
-    fprintf( fp, "variable : %d\n", j );
-  }
+  return true;
 }
 
 /* a list of keys to which callback functions are associated. */
-ZTKPrp ztk_prp_job[] = {
-  { "variable", -1, eval_variable, print_variable },
+ZTKPrp ztk_prp_key_job[] = {
+  { "variable", -1, eval_variable_key, print_variable_key },
 };
 
 void *eval_varibale_tag(void *obj, int i, void *arg, ZTK *ztk)
 {
-  printf("tag cnt=%d\n", i);
-  return ZTKEvalKey( obj, NULL, ztk, ztk_prp_job );
+  exp_obj->var_key_num[i] = ZTKCountKey( ztk, "variable" );
+  exp_obj->variable[i] = zAlloc( int, exp_obj->var_key_num[i] );
+  exp_obj->tag_cnt = i;
+  printf("tag cnt=%d/%d\n", i, exp_obj->var_tag_num-1 );
+  return ZTKEvalKey( obj, NULL, ztk, ztk_prp_key_job );
 }
 
-void print_varibale_tag(FILE*fp, int i, void *obj)
+bool print_varibale_tag(FILE*fp, int i, void *obj)
 {
-  int j;
+  ZTKPrp *copy_prp_key;
+  int prp_size;
 
-  for( j=0; j < exp_obj->var_tag_num; j++ ){
-    fprintf( fp, "[variable_tag]\n" );
-    ZTKPrpKeyFPrint( fp, obj, ztk_prp_job );
-    fprintf( fp, "\n");
-  }
+  copy_prp_key = ZTKPrpDup( ztk_prp_key_job );
+  prp_size = sizeof(ztk_prp_key_job)/sizeof(ztk_prp_key_job);
+  _ZTKPrpSetNum( copy_prp_key, prp_size, "variable", exp_obj->var_key_num[i] );
+  exp_obj->tag_cnt = i;
+  _ZTKPrpKeyFPrint( fp, obj, copy_prp_key, prp_size );
+  fprintf( fp, "\n");
+  free( copy_prp_key );
+  free( exp_obj->variable[i] );
+  return true;
 }
 
 /* a list of tags to which callback functions are associated. */
-ZTKPrp ztk_prp_calculator[] = {
+ZTKPrp ztk_prp_tag_calculator[] = {
   { "variable_tag", -1, eval_varibale_tag, print_varibale_tag}
 };
 
@@ -56,13 +67,23 @@ ZTKPrp ztk_prp_calculator[] = {
 void eval_test(ZTK *ztk)
 {
   expression_t expression;
-  /* ZTKEvalTag( &expression, NULL, ztk, ztk_prp_calculator ); */
-  ZTKEvalTag( &expression, NULL, ztk, ztk_prp_calculator );
-  expression.var_tag_num = 7;
-  expression.var_key_num = 3;
-  FILE* fp = stdout;
+  FILE *fp;
+  ZTKPrp *copy_prp_tag;
+  int prp_size;
+
+  expression.var_tag_num = ZTKCountTag( ztk, "variable_tag" );
+  expression.variable = zAlloc( int*, expression.var_tag_num );
+  expression.var_key_num = zAlloc( int, expression.var_tag_num );
+  ZTKEvalTag( &expression, NULL, ztk, ztk_prp_tag_calculator );
+  fp = stdout;
   fprintf( fp, "\n");
-  ZTKPrpTagFPrint( fp, &expression, ztk_prp_calculator );
+  copy_prp_tag = ZTKPrpDup( ztk_prp_tag_calculator );
+  prp_size = sizeof(ztk_prp_tag_calculator)/sizeof(ztk_prp_tag_calculator);
+  _ZTKPrpSetNum( copy_prp_tag, prp_size, "variable_tag", expression.var_tag_num );
+  _ZTKPrpTagFPrint( fp, &expression, copy_prp_tag, prp_size );
+  free( copy_prp_tag );
+  free( expression.var_key_num );
+  free( expression.variable );
 }
 
 int main(int argc, char *argv[])
